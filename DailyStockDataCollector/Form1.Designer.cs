c1ac2ee21b30cb98e1f8e98c9502ba55971ffdb0
Hercules <hyukkyulee@alumni.emory.edu>
2020-03-28 02:06:23 +0900

    first commit

---
+++ DailyStockDataCollector/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace DailyStockDataCollector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.투자자별매매동향Button = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dateTextBox = new System.Windows.Forms.TextBox();
+            this.프로그램매매동향Button = new System.Windows.Forms.Button();
+            this.여기서부터다시시작textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(39, 428);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(55, 17);
+            this.axKHOpenAPI1.TabIndex = 0;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(55, 157);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(570, 173);
+            this.listBox.TabIndex = 1;
+            // 
+            // 투자자별매매동향Button
+            // 
+            this.투자자별매매동향Button.Location = new System.Drawing.Point(636, 27);
+            this.투자자별매매동향Button.Name = "투자자별매매동향Button";
+            this.투자자별매매동향Button.Size = new System.Drawing.Size(152, 23);
+            this.투자자별매매동향Button.TabIndex = 2;
+            this.투자자별매매동향Button.Text = "투자자별매매동향 저장";
+            this.투자자별매매동향Button.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // dateTextBox
+            // 
+            this.dateTextBox.Location = new System.Drawing.Point(39, 30);
+            this.dateTextBox.Name = "dateTextBox";
+            this.dateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.dateTextBox.TabIndex = 4;
+            // 
+            // 프로그램매매동향Button
+            // 
+            this.프로그램매매동향Button.Location = new System.Drawing.Point(636, 65);
+            this.프로그램매매동향Button.Name = "프로그램매매동향Button";
+            this.프로그램매매동향Button.Size = new System.Drawing.Size(152, 23);
+            this.프로그램매매동향Button.TabIndex = 5;
+            this.프로그램매매동향Button.Text = "프로그램매매동향 저장";
+            this.프로그램매매동향Button.UseVisualStyleBackColor = true;
+            // 
+            // 여기서부터다시시작textBox
+            // 
+            this.여기서부터다시시작textBox.Location = new System.Drawing.Point(39, 81);
+            this.여기서부터다시시작textBox.Name = "여기서부터다시시작textBox";
+            this.여기서부터다시시작textBox.Size = new System.Drawing.Size(100, 20);
+            this.여기서부터다시시작textBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "이 종목부터 다시 시작";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "yyyyMMdd";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.여기서부터다시시작textBox);
+            this.Controls.Add(this.프로그램매매동향Button);
+            this.Controls.Add(this.dateTextBox);
+            this.Controls.Add(this.투자자별매매동향Button);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.axKHOpenAPI1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button 투자자별매매동향Button;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox dateTextBox;
+        private System.Windows.Forms.Button 프로그램매매동향Button;
+        private System.Windows.Forms.TextBox 여기서부터다시시작textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DailyStockDataCollector/Form1.cs
@@ -0,0 +1,185 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DailyStockDataCollector
+{
+    public partial class Form1 : Form
+    {
+        int screenNum = 1000;
+        string code = "";
+        static AutoResetEvent objAuto = new AutoResetEvent(false); // 이거 추가해야지 ReceiveTR 끝나고 code 업데이트 해서 안밀린다. 굳굳!!
+        public Form1()
+        {
+            InitializeComponent();
+
+            axKHOpenAPI1.CommConnect(); //start kiwoom api
+            axKHOpenAPI1.OnReceiveTrData += API_OnReceiveTrData; //TrData가 왔을 시
+
+            투자자별매매동향Button.Click += Button_Click;
+            프로그램매매동향Button.Click += Button_Click;
+
+            dateTextBox.Text = DateTime.Now.ToString("yyyyMMdd");
+            여기서부터다시시작textBox.Text = "";
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (sender == 투자자별매매동향Button)
+            {
+                listBox.Items.Add("투자자별매매동향 저장 클릭");
+                Thread rqThread = new Thread(delegate ()
+                {
+                    string[] codeList = getCodeList("kosdaq", 여기서부터다시시작textBox.Text);
+                    for (int i=0; i< codeList.Count();i++)
+                    {
+                        code = codeList[i];
+                        Console.WriteLine(axKHOpenAPI1.GetMasterCodeName(codeList[i]));
+                        axKHOpenAPI1.SetInputValue("일자", dateTextBox.Text);
+                        axKHOpenAPI1.SetInputValue("종목코드", code);
+                        axKHOpenAPI1.SetInputValue("금액수량구분", "2");
+                        axKHOpenAPI1.SetInputValue("매매구분", "0");
+                        axKHOpenAPI1.SetInputValue("단위구분", "1");
+                        //sRQName에 종목코드를 추가해서 Receive event에서도 code 받을수 있게 하기
+                        int result = axKHOpenAPI1.CommRqData("일별주가요청", "opt10059", 0, GetScreenNum());
+                        Thread.Sleep(3750);
+                        objAuto.WaitOne();
+                    }
+                });
+                rqThread.Start();
+            }
+            else if (sender == 프로그램매매동향Button)
+            {
+                listBox.Items.Add("프로그램매매동향 저장 클릭");
+                Thread rqThread = new Thread(delegate ()
+                {
+                    string[] codeList = getCodeList("kosdaq", 여기서부터다시시작textBox.Text);
+                    for (int i = 0; i < codeList.Count(); i++)
+                    {
+                        string code = codeList[i];
+                        axKHOpenAPI1.SetInputValue("시간일자구분", "2");
+                        axKHOpenAPI1.SetInputValue("금액수량구분", "2");
+                        axKHOpenAPI1.SetInputValue("종목코드", code);
+                        axKHOpenAPI1.SetInputValue("날짜", dateTextBox.Text);
+                        int result = axKHOpenAPI1.CommRqData("프로그램일별요청-" + code, "opt90013", 0, GetScreenNum());
+                        Thread.Sleep(3750);
+                        objAuto.WaitOne();
+                    }
+                });
+                rqThread.Start();
+            }
+
+        }
+        private void API_OnReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            Console.WriteLine("sErrorCode" + e.sErrorCode);
+            Console.WriteLine("sMessage" + e.sMessage);
+            Console.WriteLine("sPrevNext" + e.sPrevNext);
+            Console.WriteLine("sRecordName" + e.sRecordName);
+            Console.WriteLine("sRQName" + e.sRQName);
+            Console.WriteLine("sScrNo" + e.sScrNo);
+            Console.WriteLine("sTrCode" + e.sTrCode);
+
+
+            if (e.sRQName.Equals("일별주가요청")) 
+            {
+                listBox.Items.Add("Worked On: 일별주가요청 " + code);
+                for (int i = 0; i < axKHOpenAPI1.GetRepeatCnt(e.sTrCode, e.sRQName); i++)
+                {
+                    long recorded_time = Int64.Parse(DateTime.Now.ToString("yyyyMMddHHmmss"));
+                    int date = dataCleansing(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "일자"));
+                    int retail = dataCleansing(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "개인투자자"));
+                    int foreigner = dataCleansing(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "외국인투자자"));
+                    int insitiute = dataCleansing(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "기관계"));
+                    if (retail > 0 || foreigner > 0 || insitiute > 0)
+                    {
+                        //Console.WriteLine("recorded_time: " + recorded_time);
+                        //Console.WriteLine("Code: " + code);
+                        //Console.WriteLine("date: " + date);
+                        //Console.WriteLine("개인순매수: " + retail);
+                        //Console.WriteLine("외인순매수: " + foreigner);
+                        //Console.WriteLine("기관순매수: " + insitiute);
+                        using (IDbConnection cnn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["Default"].ConnectionString))
+                        {
+                            //이미 primary key가 있으면 ignore하고 아니면 insert하셈
+                            cnn.Execute("insert or ignore into DailyCollectedData (recorded_time, code, date, retail, foreigner, insitiute) values (@recorded_time, @code, @date, @retail, @foreigner, @insitiute)", new { recorded_time, code, date, retail, foreigner, insitiute });
+                        }
+                    }
+                    objAuto.Set();
+                }
+            }
+            else if (e.sRQName.Contains("프로그램일별요청")) 
+                // 여기서는 프로그램 수량 넣음
+            {
+                string code = e.sRQName.Split('-')[1];
+                listBox.Items.Add("Worked On: 프로그램일별요청 " + code);
+                for (int i = 0; i < axKHOpenAPI1.GetRepeatCnt(e.sTrCode, e.sRQName); i++)
+                {
+                    long recorded_time = Int64.Parse(DateTime.Now.ToString("yyyyMMddHHmmss"));
+                    int date = dataCleansing(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "일자"));
+                    int program = dataCleansing(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "프로그램순매수수량"));
+                    using (IDbConnection cnn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["Default"].ConnectionString))
+                    {
+                        //이미 primary key가 있으면 ignore하고 아니면 insert하셈
+                        cnn.Execute("update DailyCollectedData set  program = @program where (code = @code and date = @date)", new { program, code, date});
+                    }
+                }
+                objAuto.Set();
+            }
+        }
+
+
+        private int dataCleansing(string s)
+        {
+            if (s.Length > 0)
+            {
+                s = s.Trim();
+                //if (s.Substring(0, 1).Equals("-"))
+                //{
+                //    s = s.Substring(1, s.Length - 1);
+                //}
+                return Int32.Parse(s);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        
+        private string[] getCodeList(string market="kosdaq", string interruptedCode="")
+        {
+            string mkt_parameter = "";
+            if (market.Equals("kosdaq")) { mkt_parameter = "10"; }
+            else if (market.Equals("kospi")) { mkt_parameter = "0"; }
+            else { mkt_parameter = null; } // 모든 마켓
+            string[] codeArray = axKHOpenAPI1.GetCodeListByMarket(mkt_parameter).Split(';'); // 마켓에 해당하는 종목 가져오기
+            codeArray = codeArray.OrderBy(x => x).ToArray(); // Ascending
+            codeArray = codeArray.Skip(1).ToArray(); // remove first element, which is blank
+            if (interruptedCode.Equals("") == false)
+            {
+                int index = Array.FindIndex(codeArray, row => row == interruptedCode);
+                string[] segment = codeArray.Skip(index).Take(codeArray.Count()).ToArray(); // 와 이거 하는데 정말 오래걸랬다. python은 그냥 array[index:] 이러면 끝인데...
+                return segment;
+            }
+            return codeArray;
+        }
+        private string GetScreenNum()
+        {
+            if (screenNum >= 9999)
+                screenNum = 1000;
+
+            screenNum++;
+            return screenNum.ToString();
+        }
+    }
+}
